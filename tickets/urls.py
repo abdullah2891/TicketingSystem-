47d874c0572f7fb74a12ff084689e93cf958815c
Abdullah Rahman <abdullah2891@gmail.com>
2019-01-15 22:14:58 -0500

    added url slash

--- tickets/urls.py
+++ tickets/urls.py
@@ -9,10 +9,10 @@ from rest_framework.authtoken import views
 urlpatterns  = {
 	url(r'^auth', include('rest_framework.urls'), name ='rest_framework'),
 
-	url(r'^projects/$' , CreateView.as_view() , name = 'create'), 
-	url(r'^issues/$' , IssueView.as_view() , name = 'create'), 
-	url(r'^projects/(?P<pk>[0-9]+)/$' ,DetailsView.as_view() , name = "update"), 
-	url(r'^issues/(?P<pk>[0-9]+)/$' , IssueUpdateView.as_view() , name = 'update'), 
+	url(r'^projects$' , CreateView.as_view() , name = 'create'), 
+	url(r'^issues$' , IssueView.as_view() , name = 'create'), 
+	url(r'^projects(?P<pk>[0-9]+)/$' ,DetailsView.as_view() , name = "update"), 
+	url(r'^issues(?P<pk>[0-9]+)/$' , IssueUpdateView.as_view() , name = 'update'), 
 
 	url(r'^accounts', UserView.as_view() , name = 'user creation'),
 	url(r'^token/', views.obtain_auth_token)
