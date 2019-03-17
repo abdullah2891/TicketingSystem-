6de75ed604966635f1960a922cb3ba4360301be8
Abdullah Rahman <abdullah2891@gmail.com>
2019-03-17 02:17:46 -0400

    removed unique param

--- tickets/models.py
+++ tickets/models.py
@@ -10,7 +10,7 @@ from rest_framework.authtoken.models import Token
 
 
 class ProjectModel(models.Model): 
-	projects  =  models.CharField(max_length =200, blank = False , unique = True)
+	projects  =  models.CharField(max_length =200, blank = False)
 	date_created = models.DateTimeField(auto_now_add = True)
 	date_modified = models.DateTimeField(auto_now = True)
 
